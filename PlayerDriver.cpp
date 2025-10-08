ba499dd76e0c61d9a8cf3eb9299677359cff3ce9
Ashub07 <arshdeepsinghkkp07@gmail.com>
2025-10-07 22:38:51 -0400

    Update PlayerDriver.cpp

--- PlayerDriver.cpp
+++ PlayerDriver.cpp
@@ -54,7 +54,46 @@ void testPlayer() {
 
 int main() {
     cout << "----- Player Driver Test -----\n";
-    testPlayer();
+
+    cout << "Creating territories...\n";
+    std::vector<Territory*> territories;
+    std::vector<Territory*> adj;  // empty adjacency list
+    Territory* t1 = new Territory("T1", "C1", "Joe", 2, 1, &adj);
+    territories.push_back(t1);
+    Territory* t2 = new Territory("T2", "C2", "Joe", 3, 2, &adj);
+    territories.push_back(t2);
+    Territory* t3 = new Territory("T3", "C1", "Lily", 2, 3, &adj);
+    territories.push_back(t3);
+    Territory* t4 = new Territory("T4", "C2", "Joe", 1, 4, &adj);
+    territories.push_back(t4);
+    cout << "Territories created.\n";
+
+    cout << "Creating deck...\n";
+    Deck* d1 = new Deck();  // just make empty for safety
+    cout << "Deck created.\n";
+
+    cout << "Creating orders list...\n";
+    OrdersList* ordli1 = new OrdersList();
+    cout << "OrdersList created.\n";
+
+    cout << "Creating player...\n";
+    Player p1("Joe", territories, d1, ordli1);
+    cout << "Player created.\n";
+
+    cout << "Testing toDefend...\n";
+    auto defendList = p1.toDefend(p1);
+    cout << "toDefend() returned " << defendList.size() << " territories\n";
+
+    cout << "Testing toAttack...\n";
+    auto attackList = p1.toAttack(p1);
+    cout << "toAttack() returned " << attackList.size() << " territories\n";
+
+    cout << "Testing issueOrder...\n";
+    p1.issueOrder();
+    cout << "issueOrder() called successfully\n";
+
     return 0;
 }
+
+
 
